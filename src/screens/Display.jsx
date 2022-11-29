021c0a301c0459be666cdebe524029d95c719978
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-11-29 20:50:49 +0500

    Second

---
+++ src/screens/Display.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+
+
+function Display() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default Display
